4d3f8622c8b889bb954eea84fb315400f4d19585
LBang98 <dlqudgus98@gmail.com>
2024-08-05 12:07:59 +0900

    8월5일

--- src/components/game/Lobby.js
+++ src/components/game/Lobby.js
@@ -4,9 +4,26 @@ import { Stomp } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
 import './Lobby.css';
 
+const characters = [
+  { id: 1, src: '/image/character/bear.png', alt: 'Character 1' },
+  { id: 2, src: '/image/character/cow.png', alt: 'Character 2' },
+  { id: 3, src: '/image/character/dragon.png', alt: 'Character 3' },
+  { id: 4, src: '/image/character/duck.png', alt: 'Character 4' },
+  { id: 5, src: '/image/character/fox.png', alt: 'Character 5' },
+  { id: 6, src: '/image/character/monkey.png', alt: 'Character 6' },
+  { id: 7, src: '/image/character/panda.png', alt: 'Character 7' },
+  { id: 8, src: '/image/character/rabbit.png', alt: 'Character 8' },
+  { id: 9, src: '/image/character/tiger.png', alt: 'Character 9' }
+];
+
+const maps = [
+  { id: 'A', src: '/image/map/1.png', alt: 'Map A' },
+  { id: 'B', src: '/image/map/2.png', alt: 'Map B' },
+  { id: 'C', src: '/image/map/3.png', alt: 'Map C' }
+];
+
 const Lobby = () => {
   const [players, setPlayers] = useState([]);
-
   const [readyStates, setReadyStates] = useState({});
   const [allReady, setAllReady] = useState(false);
   const [isHost, setIsHost] = useState(false);
@@ -18,13 +35,19 @@ const Lobby = () => {
   const [countdown, setCountdown] = useState(3);
   const [showCountdown, setShowCountdown] = useState(false);
   const [currentStep, setCurrentStep] = useState(0);
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
+  const [selectedMap, setSelectedMap] = useState(maps[0].src);
+  const [selectedCharacters, setSelectedCharacters] = useState([]); // 선택된 캐릭터 상태 추가
 
-  const playerName = `플레이어-${Math.random().toString(36).substring(7)}`;
+  const playerName = React.useMemo(() => `${Math.random().toString(36).substring(7)}`, []);
 
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState('');
 
   useEffect(() => {
     const socket = new SockJS('http://localhost:8080/ws');
     const stompClient = Stomp.over(socket);
+
     stompClient.connect({}, () => {
       setClient(stompClient);
       stompClient.subscribe('/topic/public', (message) => {
@@ -36,7 +59,7 @@ const Lobby = () => {
         }
       });
 
-      stompClient.send('/app/chat.addUser', {}, JSON.stringify({ sender: playerName, type: 'JOIN' }));
+      stompClient.send('/app/chat.addUser', {}, JSON.stringify({ sender: playerName, type: 'JOIN', roomId: 'default' }));
     });
 
     return () => {
@@ -44,18 +67,16 @@ const Lobby = () => {
         stompClient.disconnect();
       }
     };
-  }, []);
+  }, [playerName]);
 
   const onMessageReceived = (message) => {
     if (message.type === 'JOIN') {
-      setPlayers(() => {
-        let names = message.content.split(",");
-        let prevPlayers = names.map((name) => {
-          return {name: name, ready: ""}
-        })
-
-        return prevPlayers;
+      const playersInfo = message.content.split(",");
+      const newPlayers = playersInfo.map((info) => {
+        const [name, characterSrc] = info.split(":");
+        return { name: name, ready: "", characterSrc: characterSrc || '/image/pinkbin.png' };
       });
+      setPlayers(newPlayers);
     } else if (message.type === 'READY') {
       setReadyStates(prevStates => ({
         ...prevStates,
@@ -64,57 +85,93 @@ const Lobby = () => {
       setPlayers(prevPlayers => prevPlayers.map(p => p.name === message.sender ? { ...p, ready: message.ready } : p));
     } else if (message.type === 'LEAVE') {
       setPlayers(prevPlayers => prevPlayers.filter(p => p.name !== message.sender));
+    } else if (message.type === 'SELECT') {
+      setPlayers(prevPlayers => prevPlayers.map(p => p.name === message.sender ? { ...p, characterSrc: message.content } : p));
+      setSelectedCharacters(prevSelected => [...prevSelected, message.content]);
+    } else if (message.type === 'DESELECT') {
+      setPlayers(prevPlayers => prevPlayers.map(p => p.name === message.sender ? { ...p, characterSrc: '/image/pinkbin.png' } : p));
+      setSelectedCharacters(prevSelected => prevSelected.filter(src => src !== message.content));
+    } else if (message.type === 'ERROR' && message.content === 'Character already selected') {
+      alert('The character has already been selected by another player.');
+    } else if (message.type === 'CHAT') {
+      setMessages(prevMessages => [...prevMessages, { sender: message.sender, content: message.content }]);
     }
   };
-
-
-
-
-  //준비
-  const checkAllReady = () => {
-    if (players.length > 1) {
-      const allReady = players.every(player => readyStates[player.name] === true);
-      setAllReady(allReady);
-    } else {
-      setAllReady(false);
-    }
-  };
-
-
-  useEffect(() => {
-    checkAllReady();
-  }, [players, readyStates]);
-
 
   const handleReadyClick = (player) => {
     if (client && client.connected) {
       const updatedReadyState = !player.ready;
-      client.send('/app/chat.ready', {}, JSON.stringify({ sender: player.name, type: 'READY', ready: updatedReadyState }));
+      client.send('/app/chat.ready', {}, JSON.stringify({ sender: player.name, type: 'READY', ready: updatedReadyState, roomId: 'default' }));
     }
+  };
+
+  const handleCharacterSelect = (characterId) => {
+    const characterSrc = characters.find(c => c.id === characterId).src;
+    if (selectedCharacters.includes(characterSrc)) {
+      alert('This character has already been selected by another player.');
+      return;
+    }
+    setSelectedCharacter(characterId);
+    setPlayers(prevPlayers => prevPlayers.map(player =>
+      player.name === playerName ? { ...player, characterSrc: characterSrc } : player
+    ));
+    setSelectedCharacters(prevSelected => [...prevSelected, characterSrc]); // 선택된 캐릭터 추가
+    if (client && client.connected) {
+      client.send('/app/chat.selectCharacter', {}, JSON.stringify({ sender: playerName, type: 'SELECT', content: characterSrc, roomId: 'default' }));
+    }
+  };
+
+  const handleCharacterDeselect = (characterId) => {
+    const characterSrc = characters.find(c => c.id === characterId).src;
+    setSelectedCharacter(null);
+    setPlayers(prevPlayers => prevPlayers.map(player =>
+      player.name === playerName ? { ...player, characterSrc: '/image/pinkbin.png' } : player
+    ));
+    setSelectedCharacters(prevSelected => prevSelected.filter(src => src !== characterSrc)); // 선택된 캐릭터 제거
+    if (client && client.connected) {
+      client.send('/app/chat.deselectCharacter', {}, JSON.stringify({ sender: playerName, type: 'DESELECT', content: characterSrc, roomId: 'default' }));
+    }
+  };
+
+  const handleStartGame = () => {
+    // 게임 시작 로직을 추가합니다.
+  };
+
+  const handleMapSelect = () => {
+    const currentIndex = maps.findIndex(map => map.src === selectedMap);
+    const nextIndex = (currentIndex + 1) % maps.length;
+    setSelectedMap(maps[nextIndex].src);
+  };
+
+  const sendMessage = () => {
+    const chatMessage = {
+      sender: playerName,
+      content: input,
+      type: 'CHAT'
+    };
+    client.send('/app/chat.sendMessage', {}, JSON.stringify(chatMessage));
+    setInput('');
   };
 
   return (
     <div className="backStyle">
-      <div className={"titleStyle"}>
+      <div className="titleStyle">
         <h1>Room {players.length}/4
-          <button type={"button"}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" className="bi bi-box-arrow-right" viewBox="0 0 16 16">
-              <path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5.5 0 0 0 0 3.5v9A1.5.1.5 0 0 0 1.5 14h8a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-1 0z"/>
-              <path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
-            </svg>
+          <button type="button">
+            <h3>Exit</h3>
           </button>
         </h1>
       </div>
 
-      {!showLadder && players.length > 0 && ( // 사다리 게임이 표시되지 않은 경우에만 렌더링
+      {!showLadder && players.length > 0 && (
         <div className="formStyle">
-          {players.map((prevPlayers, index) => (
+          {players.map((player, index) => (
             <div key={index} className="cardStyle">
-              <h2>{prevPlayers.name}</h2>
-              <img src="/image/pinkbin.png" alt={prevPlayers.name} />
+              <h2>{player.name}</h2>
+              <img src={player.characterSrc} alt={player.name} />
               {index !== 0 && (
-                <button type="button" onClick={() => handleReadyClick(prevPlayers)}>
-                  {prevPlayers.ready ? '준비완료' : '준비'}
+                <button type="button" onClick={() => handleReadyClick(player)}>
+                  {player.ready ? '준비완료' : '준비'}
                 </button>
               )}
             </div>
@@ -122,48 +179,91 @@ const Lobby = () => {
         </div>
       )}
 
-      {/*isHost &&*/ allReady && showStartButton && ( // 게임 시작 버튼이 표시될 때만 렌더링
-        <div className="startGame">
-          <button type="button" onClick={handleStartGame}>게임 시작하기</button>
+      <div className="chatContainer">
+        <div className="chatMessages">
+          {messages.map((msg, index) => (
+            <div key={index}>
+              <b>{msg.sender}: </b>{msg.content}
+            </div>
+          ))}
+        </div>
+        <div className="chatInputContainer">
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                sendMessage();
+              }
+            }}
+            className="chatInput"
+            placeholder="Type your message..."
+          />
+          <button onClick={sendMessage} className="chatSendButton">
+            Send
+          </button>
+        </div>
+      </div>
+
+      <div className="game-container">
+        <b style={{ fontSize: '20px' }}>Select&nbsp;Character</b>
+        <div className="character-selection">
+          {characters.map((character) => (
+            <div key={character.id} className="character-slot">
+              <img
+                src={character.src}
+                alt={character.alt}
+                className={`character ${selectedCharacter === character.id ? 'selected' : ''}`}
+                onClick={() => {
+                  if (selectedCharacter === character.id) {
+                    handleCharacterDeselect(character.id);
+                  } else if (!selectedCharacters.includes(character.src)) {
+                    handleCharacterSelect(character.id);
+                  }
+                }}
+              />
+              {selectedCharacters.includes(character.src) && (
+                <div className="character-overlay">X</div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="map-info">
+        <img src={selectedMap} alt="Map" className="map-image" />
+        <div className="map-details">
+        </div>
+        <button className="map-select-button" onClick={handleMapSelect}>맵 변경</button>
+      </div>
+
+      {isHost && allReady && showStartButton && (
+        <div className="start-button-section">
+          <button className="start-button" onClick={handleStartGame}>시작</button>
         </div>
       )}
 
-      {showCountdown && ( // 카운트다운 표시
+      {showCountdown && (
         <div className="countdown">
           <h2>{countdown}</h2>
         </div>
       )}
 
-      {showLadder && ( // 사다리 게임 표시 여부에 따라 렌더링
+      {showLadder && (
         <div className="ladder-container">
           {ladder.map((row, rowIndex) => (
             <div key={rowIndex} className="ladder-row">
               {row.map((cell, cellIndex) => (
-                <div key={cellIndex} className={`ladder-cell ${cell ? 'connected' : ''} ${currentStep === rowIndex ? 'current-step' : ''}`}></div>
+                <div key={cellIndex}
+                     className={`ladder-cell ${cell ? 'connected' : ''} ${currentStep === rowIndex ? 'current-step' : ''}`}></div>
               ))}
             </div>
           ))}
         </div>
       )}
 
-      <div className="chatContainer">
-        <div className="chatMessages">
-          <h5>핑크빈 : ㅎㅇㅎㅇ</h5>
-          <h5>핑크빈 : ㅎㅇㅎㅇ</h5>
-          <h5>핑크빈 : ㅎㅇㅎㅇ</h5>
-          <h5>핑크빈 : ㅎㅇㅎㅇ</h5>
-        </div>
-        <div className="chatInputContainer">
-          <input
-            placeholder="Type your message..."
-            className="chatInput" />
-          <button variant="solid" className="chatSendButton">
-            Send
-          </button>
-        </div>
-      </div>
-
-      {playerOrder.length > 0 && showLadder && ( // 사다리 게임이 표시된 후에만 렌더링
+      {playerOrder.length > 0 && showLadder && (
         <div className="orderList">
           <h2>플레이어 순서</h2>
           <ol>
