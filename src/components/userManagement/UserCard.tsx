42fb1dead7523e4c7979b8fcba31a9ac476f65d6
minseok0507 <jms892812@gmail.com>
2024-09-05 11:51:56 +0900

    readme del type error fix

--- src/components/userManagement/UserCard.tsx
+++ src/components/userManagement/UserCard.tsx
@@ -1,37 +1,37 @@
 import {Box, Card, Divider, Grid, styled} from "@mui/material";
-import {H3, H5, Small, Tiny} from "../Typography";
+import {H3, H5, Small} from "../Typography";
 import React, {FC, useEffect, useState} from "react";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
-import {usePathname} from "next/navigation";
 
 // component props interface
 interface UserCardProps {
-    user: {
-        name: string;
-        roomName: string;
-        seedMoney: number;
-        fullMember: number;
-        currentMember: number;
-        roomCode: string;
-        cover: string;
-    };
+  user: {
+    name: string;
+    roomName: string;
+    seedMoney: number;
+    fullMember: number;
+    currentMember: number;
+    roomCode: string;
+    cover: string;
+  };
+  roomStatus: boolean; // 또는 적절한 타입으로 수정
 }
 
 // styled components
 const ImageWrapper = styled(Box)(({theme}) => ({
-    height: 100,
-    position: "relative",
-    "&::before": {
-        content: '""',
-        width: "100%",
-        height: "100%",
-        left: 0,
-        top: 0,
-        position: "absolute",
-        opacity: 0.6,
-        backgroundColor: theme.palette.primary.light,
-    },
+  height: 100,
+  position: "relative",
+  "&::before": {
+    content: '""',
+    width: "100%",
+    height: "100%",
+    left: 0,
+    top: 0,
+    position: "absolute",
+    opacity: 0.6,
+    backgroundColor: theme.palette.primary.light,
+  },
 }));
 
 const StyledAvatar = styled(Avatar)(({theme}) => ({
@@ -49,7 +49,7 @@ const StyledAvatar = styled(Avatar)(({theme}) => ({
 }));
 
 
-const UserCard: FC<UserCardProps> = ({user,roomStatus}) => {
+const UserCard: FC<UserCardProps> = ({user, roomStatus}) => {
   const [goURL, setGoURL] = useState('');
 
 // 경로가 변경될 때 apiURL을 업데이트
@@ -61,55 +61,55 @@ const UserCard: FC<UserCardProps> = ({user,roomStatus}) => {
     }
   }, [roomStatus]);
 
-    return (
-        <Card onClick={() => {
-            window.open(`${goURL}${user.roomCode}`, '_blank')
+  return (
+    <Card onClick={() => {
+      window.open(`${goURL}${user.roomCode}`, '_blank')
+    }}>
+      <ImageWrapper>
+        <Typography sx={{
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          width: "100%",
+          textAlign: "center",
+          transform: "translate(-50%, -50%)",
         }}>
-            <ImageWrapper>
-              <Typography sx={{
-                position: "absolute",
-                left: "50%",
-                top: "50%",
-                width: "100%",
-                textAlign: "center",
-                transform: "translate(-50%, -50%)",
-              }}>
-                {user.roomName}
-              </Typography>
-              <StyledAvatar src={user.cover} alt={user.name}/>
-            </ImageWrapper>
+          {user.roomName}
+        </Typography>
+        <StyledAvatar src={user.cover} alt={user.name}/>
+      </ImageWrapper>
 
-            <Box
-                sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    flexDirection: "column",
-                    marginTop: 5,
-                }}
-            >
-                <H5><span style={{fontWeight: "bolder"}}>{user.name}</span>님의 방</H5>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          marginTop: 5,
+        }}
+      >
+        <H5><span style={{fontWeight: "bolder"}}>{user.name}</span>님의 방</H5>
 
 
-            </Box>
+      </Box>
 
-            <Divider sx={{my: 2}}/>
+      <Divider sx={{my: 2}}/>
 
-            <Grid container spacing={3} mb={2}>
-                <Grid item xs={6} textAlign="center">
-                    <H3>{user.currentMember}/{user.fullMember}</H3>
-                    <Small color="text.disabled">인원 수</Small>
-                </Grid>
-                {/*<Grid item xs={4} textAlign="center">*/}
-                {/*  <H3>{user.follower}</H3>*/}
-                {/*  <Small color="text.disabled">Followers</Small>*/}
-                {/*</Grid>*/}
-                <Grid item xs={6} textAlign="center">
-                    <H3>{user.seedMoney}</H3>
-                    <Small color="text.disabled">시드머니</Small>
-                </Grid>
-            </Grid>
-        </Card>
-    );
+      <Grid container spacing={3} mb={2}>
+        <Grid item xs={6} textAlign="center">
+          <H3>{user.currentMember}/{user.fullMember}</H3>
+          <Small color="text.disabled">인원 수</Small>
+        </Grid>
+        {/*<Grid item xs={4} textAlign="center">*/}
+        {/*  <H3>{user.follower}</H3>*/}
+        {/*  <Small color="text.disabled">Followers</Small>*/}
+        {/*</Grid>*/}
+        <Grid item xs={6} textAlign="center">
+          <H3>{user.seedMoney}</H3>
+          <Small color="text.disabled">시드머니</Small>
+        </Grid>
+      </Grid>
+    </Card>
+  );
 };
 
 export default UserCard;
